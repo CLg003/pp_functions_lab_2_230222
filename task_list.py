f690d7faddf5a2bbb2c02e1dd8df0f992dceea4f
CLg003 <clairemjlaing@gmail.com>
2022-02-23 15:31:39 +0000

    question 1

--- task_list.py
+++ task_list.py
@@ -5,3 +5,12 @@ tasks = [
     { "description": "Feed Cat", "completed": False, "time_taken": 5 },
     { "description": "Walk Dog", "completed": True, "time_taken": 60 },
 ]
+# Print a list of uncompleted tasks
+def uncompleted_tasks(task_list):
+    uncompleted_task_list = []
+    for task in task_list:
+        if task["completed"] == False:
+            uncompleted_task_list.append(task["description"])
+    print(uncompleted_task_list)
+
+uncompleted_tasks(tasks)
